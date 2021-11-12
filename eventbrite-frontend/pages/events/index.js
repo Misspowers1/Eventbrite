f0619c6a3f79193a9a00b2e7c7b3754bdc9a4d80
Misspowers1 <misspowers01@gmail.com>
2021-11-12 13:39:32 +0300

    may this code be with you

---
+++ eventbrite-frontend/pages/events/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function EventPage() {
+  return (
+    <Layout title="Events">
+      <h1>My Events</h1>
+    </Layout>
+  );
+}
